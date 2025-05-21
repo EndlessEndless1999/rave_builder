a00d6eb460b386e1a67c4317566759f0c779e361
Charlie Mitchell <115580045+EndlessEndless1999@users.noreply.github.com>
2025-05-21 14:09:42 +0100

    add drag and drop to audio

--- app.go
+++ app.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
+	"os"
 )
 
 // App struct
 type App struct {
 	ctx context.Context
+}
+
+// FileData Struct
+type FileData struct {
+	Base64   string `json:"base64"`
+	MimeType string `json:"mimeType"`
 }
 
 // NewApp creates a new App application struct
@@ -24,4 +32,13 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) ReadFile(path string) string {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return ""
+	}
+
+	return base64.StdEncoding.EncodeToString(data)
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,49 @@
 // import { useState } from 'react'
+import { useEffect, useState } from "react"
 import { Button } from "./components/ui/button"
+import { OnFileDrop} from "../wailsjs/runtime"
+import {ReadFile} from "../wailsjs/go/main/App"
 
 
 function App() {
 
+  const [base64Audio, setBase64Audio] = useState<string | null>(null);
+
+  useEffect(() => {
+    OnFileDrop((_, __, paths) => {
+
+      const path = paths[0];
+
+      ReadFile(path).then(data => {
+        setBase64Audio(data)
+      })
+    }, true);
+  },[])
+
   return (
+    // Replace Image Tags with Audio Tags
     <>
-      <h1 className='text-red-500'>Hello</h1>
-    <Button>Hiya</Button>
+      <div className="flex flex-col h-dvh w-dvw p-4 gap-4">
+        <div className="flex flex-row h-1/2 w-full gap-4">
+          <div className="border rounded overflow-hidden w-1/2"
+          style={{"--wails-drop-target": "drop"} as React.CSSProperties}>
+
+          {base64Audio ? (
+            <audio controls className="w-full">
+              <source src={`data:audio/mpeg;base64,${base64Audio}`} type="audio/mpeg" />
+              Your browser does not support the audio element.
+            </audio>
+          ) : (
+            <p className="text-center">Drop an audio file here</p>
+          )}
+          </div>
+          
+          <div className="border rounded overflow-hidden w-1/2"></div>
+        </div>
+
+
+        <Button>Play Song</Button>
+      </div>
     </>
   )
 }

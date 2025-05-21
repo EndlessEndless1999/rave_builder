952b91acf8e481ee424b7b074a752d247fa7ae5a
Charlie Mitchell <115580045+EndlessEndless1999@users.noreply.github.com>
2025-05-21 09:11:15 +0100

    setup shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,15 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+// import { useState } from 'react'
+import { Button } from "./components/ui/button"
+
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
-
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-        </div>
-    )
+  return (
+    <>
+      <h1 className='text-red-500'>Hello</h1>
+    <Button>Hiya</Button>
+    </>
+  )
 }
 
 export default App
